799d655719bc33007a232b6a590bf3d1f91b2ded
Daniel Agbay <dagbay@redhat.com>
2022-05-25 18:48:10 -0500

    updated message format

--- static_responses.py
+++ static_responses.py
@@ -1,9 +1,8 @@
 help = "Welcome to Everybotty! \
     If you would like to chat with a bot, select a personality or send a prompt. \
-    Reply /help to see this message, or /personalities to see available personalities. \
-    \
-    To chat with a personality use the following format: <personality> <message>.  \
-    To chat with a random personality use the following format: <message>. \
+    Reply /help to see this message, or /personalities to see available personalities. \n\
+    To chat with a personality use the following format: <personality> <message>.\n \
+    To chat with a random personality use the following format: <message>. \n\
     Examples: 'lonely-bot I'm so alone :(', 'How are you today?'"
 
 personalities_prefix = "The available personalities are: "
