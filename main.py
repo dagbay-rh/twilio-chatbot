4388ffd2578a15d173d1010a8ee8e275bf6c2043
Daniel Agbay <dagbay@redhat.com>
2022-05-25 12:49:04 -0500

    printing twilio request

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from pprint import pprint
 from flask import Flask, request, session
+import requests
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    query = request.form['Body']
+GPT3_CHATBOT_URL="https://gpt-chatbot-api.herokuapp.com/"
 
-    pprint(query)
+@app.route("/api/chatbot", methods=['POST'])
+def sms_reply():
+    print("Request form: ", end="")
+    pprint(request.form)
+
+    personalities = get_personalities()
+    pprint(personalities.json())
+
+    incoming_sms = request.form['Body']
+
+    # bot_response = requests.post(GPT3_CHATBOT_URL + "/api/chat", )
 
     resp = MessagingResponse()
-    resp.message("PANIK: " + query)
+    resp.message("PANIK: " + incoming_sms)
 
-    return str(resp)
+    return str(resp), 200
+
+def get_personalities():
+    return requests.get(GPT3_CHATBOT_URL + "api/personalities")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
