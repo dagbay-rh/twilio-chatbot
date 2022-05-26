0ab23ff7c2ce27da017fe30e05507e51612a2e19
Daniel Agbay <dagbay@redhat.com>
2022-05-25 20:03:53 -0500

    updated help message

--- static_responses.py
+++ static_responses.py
@@ -1,8 +1,8 @@
 help = """
 Welcome to Everybotty! If you would like to chat with a bot, select a personality or send a message.\n
-Available commands: /help to see this message, /personalities to see available personalities, /personality <...> to set a personality.\n
+Available commands: /help to see this message, /personalities to see available personalities, /personality <...> to set a personality, /show to show your set personality.\n
 If you start chatting without selecting a personality, a random one will be chosen for you!\n
-Example messages: '/help', '/personality lonely-bot', 'How are you today?'
+Example messages: '/help', '/personality lonely-bot', '/show', 'How are you today?'
 """
 
 personalities_prefix = "The available personalities are: "
